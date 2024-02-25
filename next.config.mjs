319279fda2aa6a309e6f2d14023f1a8bc3adf8d7
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-02-26 02:05:27 +0530

    worked on Contact an ABout page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "https://media.sproutsocial.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
